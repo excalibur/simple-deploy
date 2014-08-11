e883b381dd3b759188e5bab00f5d65f5a19666d2
excalibur <excalibur-faith@qq.com>
2014-08-11 17:09:25 +0800

    first commit

---
+++ bin/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 设置部署环境
+select env in "prod" "dev"
+do
+case $env in
+"prod")
+    echo -e "[INFO] set environment to \033[32m$env\033[0m"
+    ;;
+"dev")
+    echo "stop something"  
+    ;;
+*)
+    echo "Ignorant"  
+    ;;
+esac
+done;
